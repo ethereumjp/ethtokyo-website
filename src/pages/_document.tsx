7be14ea5e6ce85c0197f16a9c92df22dfcc5591b
neila <40727091+neila@users.noreply.github.com>
2024-01-18 19:03:14 +0900

    set og image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,7 @@ const CustomDocument = (props: NoncedDocument) => {
         <meta property="og:title" content="ETHTokyo 2024" />
         <meta
           property="og:image"
-          content="https://www.ethtokyo.com/images/hero-top.png"
+          content="https://www.ethtokyo.com/images/hero_top.jpg"
         />
         <meta
           property="og:description"
