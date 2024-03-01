be5f5ffe86c5a3fa1a08538d3c20eb926c15ebf3
neila <40727091+neila@users.noreply.github.com>
2024-03-01 16:29:10 +0900

    export

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n: { locales: ["en", "ja"], defaultLocale: "en", localeDetection: false },
+  // i18n: { locales: ["en", "ja"], defaultLocale: "en", localeDetection: false },
   compiler: {
     emotion: true,
   },
+  output: "export",
 };
 
 module.exports = nextConfig;
