40ab69fc10d0fbe3a2af1755eab5ea266ba237eb
neila <40727091+neila@users.noreply.github.com>
2024-03-15 02:03:45 +0900

    static

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: { unoptimized: true },
   reactStrictMode: true,
   // i18n: { locales: ["en", "ja"], defaultLocale: "en", localeDetection: false },
   compiler: {
