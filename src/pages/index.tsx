c6f9a2cf0f3bbb80d3201f7af99d08ed52b87a29
neila <40727091+neila@users.noreply.github.com>
2024-03-20 18:17:22 +0900

    add counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layouts/base";
 import ScheduleSection from "@/components/organisms/Schedule";
 import StatementSection from "@/components/organisms/Statement";
-import { brand } from "@/themes/settings/color";
+import { brand, neutral } from "@/themes/settings/color";
 import type { PageProps } from "@/types";
 import type { ComponentProps } from "@/types";
 import { css } from "@emotion/react";
@@ -10,6 +10,8 @@ import Image from "next/image";
 import topHero from "public/images/hero_top.jpg";
 import seikaiha from "public/images/seikaiha.png";
 import ETHTokyoLogo from "public/logo/ETHTokyoLogo.png";
+
+import { useEffect, useState } from "react";
 
 const Page: NextPage<PageProps> = ({ params, searchParams }) => {
   const TriangleJpWaveBackground = ({ children }: ComponentProps) => {
@@ -24,9 +26,8 @@ const Page: NextPage<PageProps> = ({ params, searchParams }) => {
       clip-path: polygon(calc(50% - 380px) 0, calc(50% + 380px) 0, 50% 500px);
       height: 500px;
       text-align: center;
-
-
     `;
+
     return (
       <div css={jpWaveBackgroundStyle}>
         <div
@@ -66,6 +67,91 @@ const Page: NextPage<PageProps> = ({ params, searchParams }) => {
       z-index: -1;
     `;
 
+    const CountdownPanel = () => {
+      const panelWrapperStyle = css`
+        display: flex;
+        justify-content: space-evenly;
+        padding: 4rem;
+        text-align: center;
+      `;
+
+      const numberPanelStyle = css`
+        background-color: ${brand.Miyabi};
+        background-color: #552266BF;
+        border-radius: 1rem;
+        color: ${neutral.White};
+        font-size: 8rem;
+        font-weight: 600;
+        padding: 0 1rem;
+      `;
+
+      const countDown = (target: number, now: number) => {
+        const diffTime = target - now;
+
+        const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        );
+        const minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+
+        return [days, hours, minutes, seconds];
+      };
+
+      const [now, setNow] = useState(new Date().getTime());
+      useEffect(() => {
+        const secTimer = setInterval(() => {
+          setNow(new Date().getTime());
+        }, 1000);
+
+        return () => clearInterval(secTimer);
+      }, []);
+
+      const eventDate = new Date("Aug 23, 2024").getTime();
+      const [days, hrs, mins, secs] = countDown(eventDate, now);
+
+      return (
+        <div css={panelWrapperStyle}>
+          <div css={numberPanelStyle}>
+            {days.toLocaleString(undefined, { minimumIntegerDigits: 2 })}
+            <hr
+              css={css`border: 0; border-bottom: 1px solid white; height: 1px; margin:0;`}
+            />
+            <p css={css`font-size: 2rem; font-weight: 400; margin:1rem;`}>
+              days
+            </p>
+          </div>
+          <div css={numberPanelStyle}>
+            {hrs.toLocaleString(undefined, { minimumIntegerDigits: 2 })}
+            <hr
+              css={css`border: 0; border-bottom: 1px solid white; height: 1px; margin:0;`}
+            />
+            <p css={css`font-size: 2rem; font-weight: 400; margin:1rem;`}>
+              hours
+            </p>
+          </div>
+          <div css={numberPanelStyle}>
+            {mins.toLocaleString(undefined, { minimumIntegerDigits: 2 })}
+            <hr
+              css={css`border: 0; border-bottom: 1px solid white; height: 1px; margin:0;`}
+            />
+            <p css={css`font-size: 2rem; font-weight: 400; margin:1rem;`}>
+              minutes
+            </p>
+          </div>
+          <div css={numberPanelStyle}>
+            {secs.toLocaleString(undefined, { minimumIntegerDigits: 2 })}
+            <hr
+              css={css`border: 0; border-bottom: 1px solid white; height: 1px; margin:0;`}
+            />
+            <p css={css`font-size: 2rem; font-weight: 400; margin:1rem;`}>
+              seconds
+            </p>
+          </div>
+        </div>
+      );
+    };
+
     return (
       <section id="top" css={topBackgroundStyle}>
         <TriangleJpWaveBackground>
@@ -79,6 +165,7 @@ const Page: NextPage<PageProps> = ({ params, searchParams }) => {
             <h1>Tokyo, Japan</h1>
           </div>
         </TriangleJpWaveBackground>
+        <CountdownPanel />
       </section>
     );
   };
