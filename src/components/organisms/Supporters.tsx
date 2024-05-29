011b45335ce0d7b8de85540f4bb77d8ed2f14159
neila <40727091+neila@users.noreply.github.com>
2024-05-29 20:41:17 +0900

    add cabinet and esp

--- src/components/organisms/Supporters.tsx
+++ src/components/organisms/Supporters.tsx
@@ -2,13 +2,14 @@ import { mq } from "@/themes/settings/breakpoints";
 import { brand, neutral } from "@/themes/settings/color";
 import { css } from "@emotion/react";
 import Image, { StaticImageData } from "next/image";
-import seikaiha from "public/images/seikaiha.png";
 import AkindoLogo from "public/logo/AkindoLogo.svg";
 import BunzzLogo from "public/logo/BunzzLogo.svg";
+import CabinetLogo from "public/logo/CabinetLogo.png";
 import CentrumLogo from "public/logo/CentrumLogo.svg";
 import CoinpostLogo from "public/logo/CoinpostLogo.png";
 import EFLogo from "public/logo/EFLogo.png";
 import EJLogoFace from "public/logo/EJLogoFace_dark.png";
+import ESPLogo from "public/logo/ESPLogo.svg";
 import ETHRiyadhLogo from "public/logo/ETHRiyadhLogo.svg";
 import ETHTaipeiLogo from "public/logo/ETHTaipeiLogo.png";
 import FractonLogo from "public/logo/FractonLogo.png";
@@ -20,46 +21,42 @@ import ZeloLogo from "public/logo/ZeloLogo.png";
 const SupportersSection = () => {
   const SupporterLogo: React.FC<{
     source: StaticImageData;
-    text: string;
     link: string;
-  }> = ({ source, text, link }) => (
-    <a href={link} target="_blank" rel="noreferrer">
-      <Image
-        src={source}
-        alt={text}
-        css={css`
+    text: string;
+    tier: "platinum" | "gold" | "silver" | "bronze" | "partner";
+  }> = ({ source, text, link, tier }) => {
+    const sizes = {
+      platinum: 172,
+      gold: 156,
+      silver: 144,
+      bronze: 132,
+      partner: 120,
+    };
+    return (
+      <a href={link} target="_blank" rel="noreferrer">
+        <Image
+          src={source}
+          alt={text}
+          css={css`
             height: auto;
-            width:128px;
+            width: ${sizes[tier]}px;
 
             ${mq.laptop}{
-              max-height: 128px;
-              width: 256px;
+              max-height: ${sizes[tier]}px;
+              width: ${sizes[tier] * 2}px;
             }
           `}
-      />
-    </a>
-  );
+        />
+      </a>
+    );
+  };
 
   const SponsorTier: React.FC<{
-    tier: "platinum" | "gold" | "silver" | "bronze";
     children?: React.ReactNode;
-  }> = ({ tier, children }) => {
-    const tierColor = {
-      platinum: "#e5e4e255",
-      gold: "#FFD70055",
-      silver: "#c0c0c055",
-      bronze: "#cd7f3255",
-    };
-
+  }> = ({ children }) => {
     return (
       <div
         css={css`
-          background-color: ${tierColor[tier]};
-          background-image: url(${seikaiha.src});
-          background-position: center;
-          background-repeat: repeat;
-          background-size: cover;
-          border-radius: 0.75rem;
           padding: 1rem 0.5rem;
 
           ${mq.laptop}{
@@ -132,11 +129,26 @@ const SupportersSection = () => {
         >
           Sponsored by
         </h2>
-        <SponsorTier tier="platinum">
+        <SponsorTier>
           <SupporterLogo
             source={GMOLogo}
             text="GMO Internet Group, Inc."
             link="https://www.gmo.jp/"
+            tier="platinum"
+          />
+          <SupporterLogo
+            source={CabinetLogo}
+            text="Cabinet, Inc."
+            link="https://cbnt.co.jp/"
+            tier="platinum"
+          />
+        </SponsorTier>
+        <SponsorTier>
+          <SupporterLogo
+            source={ESPLogo}
+            text="ESP"
+            link="https://esp.ethereum.foundation/"
+            tier="gold"
           />
         </SponsorTier>
       </section>
@@ -147,56 +159,67 @@ const SupportersSection = () => {
             source={ShibuyaLogo}
             text="Shibuya City"
             link="https://www.city.shibuya.tokyo.jp/"
+            tier="partner"
           />
           <SupporterLogo
             source={EFLogo}
             text="Ethereum Foundation"
             link="https://ethereum.foundation/"
+            tier="partner"
           />
           <SupporterLogo
             source={ETHTaipeiLogo}
             text="ETH Taipei"
             link="https://ethtaipei.org/"
+            tier="partner"
           />
           <SupporterLogo
             source={ETHRiyadhLogo}
             text="ETH Riyadh"
             link="https://ethriyadh.com/"
+            tier="partner"
           />
           <SupporterLogo
             source={FractonLogo}
             text="Fracton Ventures"
             link="https://fracton.ventures/"
+            tier="partner"
           />
           <SupporterLogo
             source={CoinpostLogo}
             text="Coinpost"
             link="https://coinpost.jp/"
+            tier="partner"
           />
           <SupporterLogo
             source={AkindoLogo}
             text="Akindo"
             link="https://akindo.io/"
+            tier="partner"
           />
           <SupporterLogo
             source={CentrumLogo}
             text="Centrum"
             link="https://centrum.studio/"
+            tier="partner"
           />
           <SupporterLogo
             source={BunzzLogo}
             text="Bunzz"
             link="https://www.bunzz.dev/"
+            tier="partner"
           />
           <SupporterLogo
             source={TangemLogo}
             text="Tangem"
             link="https://tangem.com/"
+            tier="partner"
           />
           <SupporterLogo
             source={ZeloLogo}
             text="Zelo"
             link="https://zelojapan.com/"
+            tier="partner"
           />
         </PartnerCategory>
         <p>
