fa7e75146b817b6459e89b736941c66c8eda2b9e
neila <40727091+neila@users.noreply.github.com>
2025-04-11 16:10:25 +0900

    add env

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: { unoptimized: true },
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
   // i18n: { locales: ["en", "ja"], defaultLocale: "en", localeDetection: false },
   compiler: {
     emotion: true,
   },
-  output: "export",
+  env: {
+    NEXT_PUBLIC_AIRTABLE_PAT: process.env.NEXT_PUBLIC_AIRTABLE_PAT,
+    NEXT_PUBLIC_AIRTABLE_BASE_INQUIRYFORM:
+      process.env.NEXT_PUBLIC_AIRTABLE_BASE_INQUIRYFORM,
+    NEXT_PUBLIC_AIRTABLE_TABLE_INQUIRYFORM:
+      process.env.NEXT_PUBLIC_AIRTABLE_TABLE_INQUIRYFORM,
+  },
 };
 
 module.exports = nextConfig;
