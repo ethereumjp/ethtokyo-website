464bf065ba4b32693867b57a86f508144f565dee
Seiya <silmin4@proton.me>
2025-06-16 15:19:24 +0900

    fix importing order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Layout from "@/components/layouts/base";
 import AboutSection from "@/components/organisms/AboutSection";
-import TicketSection from "@/components/organisms/TicketSection";
 import ConferenceSection from "@/components/organisms/ConferenceSection";
 import HeroSection from "@/components/organisms/HeroSection";
 import ScheduleSection from "@/components/organisms/ScheduleSection";
+import TicketSection from "@/components/organisms/TicketSection";
 import TracksSection from "@/components/organisms/TracksSection";
 import VenueSection from "@/components/organisms/VenueSection";
 import type { PageProps } from "@/types";
