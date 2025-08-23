ca42a05510fb21a8184a05c3304636e91118a94b
yosuke <yosuke2002jp@yahoo.co.jp>
2025-08-24 08:37:29 +0900

    Revert "modify package.json"
    This reverts commit 1ff208b1e1206ba603192d62b6df02116dfcaa8e.

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  // i18n: { locales: ["en", "ja"], defaultLocale: "en", localeDetection: false },
+  compiler: {
+    emotion: true,
+  },
+  env: {
+    NEXT_PUBLIC_AIRTABLE_PAT: process.env.NEXT_PUBLIC_AIRTABLE_PAT,
+    NEXT_PUBLIC_AIRTABLE_BASE: process.env.NEXT_PUBLIC_AIRTABLE_BASE,
+    NEXT_PUBLIC_AIRTABLE_TABLE: process.env.NEXT_PUBLIC_AIRTABLE_TABLE,
+  },
+};
+
+export default nextConfig;
